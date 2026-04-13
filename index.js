e214408cf461c74e28da422f710d9fdb323a6f8a
khushpatel00 <patelxkhush@gmail.com>
2026-04-13 14:22:56 +0530

    logs on connect-disconnect fixed: duplication on frontend

--- index.js
+++ index.js
@@ -10,14 +10,20 @@ const io = socketio(socketServer, {
 })
 
 io.on('connection', (socket) => {
-    console.log(socket.id)
+    console.log('connected with: ', socket.id)
+    socket.broadcast.emit('user-connected', { id: socket.id });
     socket.on('send-devinfo', (data) => {
-        console.log(data)
+        console.log(data);
     })
     socket.on('send-message', (data) => {
-        io.emit('recieve-new-message', data);
+        console.log(data);
+        socket.broadcast.emit('recieve-new-message', data);
+    });
+    socket.on('disconnect', (reas) => {
+        console.log(`disconnected to: ${socket.id} ${reas} `)
+        socket.broadcast.emit('user-left', { id: socket.id });
     })
-})
+});
 
 socketServer.listen(8080, () => {
     console.log('listening on port 8080');
